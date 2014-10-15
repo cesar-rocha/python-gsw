ff184643cd74855c70c98f89bf857241d6b39e6e
ocefpaf <ocefpaf@gmail.com>
2014-10-14 22:32:35 -0300

    Make it a function to avoid nosetests clash.

--- gsw/utilities/list_npz.py
+++ gsw/utilities/list_npz.py
@@ -5,22 +5,21 @@ List to stdout the contents of an npz file used in testing.
 The filename is the sole command-line argument.
 """
 
-import sys
 import numpy as np
 
-fname = sys.argv[1]
 
-dat = np.load(fname)
-keys = dat.keys()
-keys.sort()
-klens = [len(str(k)) for k in keys]
-klen = max(klens)
+def list_npz(fname):
+    dat = np.load(fname)
+    keys = dat.keys()
+    keys.sort()
+    klens = [len(str(k)) for k in keys]
+    klen = max(klens)
+    str_fmt = "{0!s:<{klen}} : {1!s:>10}  {2!s:>12}\n"
+    slist = [str_fmt.format(k, dat[k].dtype, dat[k].shape, klen=klen)
+             for k in keys]
+    print(''.join(slist))
 
-str_fmt = "{0!s:<{klen}} : {1!s:>10}  {2!s:>12}\n"
-
-slist = [str_fmt.format(k, dat[k].dtype, dat[k].shape, klen=klen)
-         for k in keys]
-
-print ''.join(slist)
-
-
+if __name__ == '__main__':
+    import sys
+    fname = sys.argv[1]
+    list_npz(fname)
